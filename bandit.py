629c5e11dfec981cf71aafd5418eae2277404f62
Nikolas Schriefer <info@nikolasschriefer.de>
2019-04-28 15:14:00 +0200

    Add docstrings

--- bandit.py
+++ bandit.py
@@ -3,6 +3,11 @@ from scipy.stats import beta
 
 
 class Bandit():
+    """
+    Bandit instance holds cumulated trials and successes
+    as well as "memorized" periodic results if specified;
+    expects num_options and prior tuple to initialize
+    """
     def __init__(self, num_options, memory=False,
                  shape='constant', cutoff=28):
         self.memory = memory
@@ -16,12 +21,18 @@ class Bandit():
             self.cutoff = cutoff
 
     def add_period(self):
+        """
+        Add new empty period to memory
+        """
         self.periods['trials'].append(
             np.zeros(shape=(self.num_options,), dtype=int))
         self.periods['successes'].append(
             np.zeros(shape=(self.num_options,), dtype=int))
 
     def add_results(self, option_id, trials, successes):
+        """
+        Add trials and successes from last period to one option
+        """
         self.trials[option_id] += trials
         self.successes[option_id] += successes
         if self.memory:
@@ -29,9 +40,16 @@ class Bandit():
             self.periods['successes'][-1][option_id] += successes
 
     def weigh_options(self):
+        """
+        Weigh options for current period's choice based on distance from now
+        with alternatively shaped discount functions
+        """
+        # Initialize trial and success weights for each option
         trial_weights = np.zeros(shape=(self.num_options,), dtype=float)
         success_weights = np.zeros(shape=(self.num_options,), dtype=float)
         num_periods = len(self.periods['trials'])
+        # Loop over each period and option and
+        # determine and add respective weights
         for period_id in range(num_periods):
             distance = num_periods - period_id
             for option_id in range(self.num_options):
@@ -39,18 +57,28 @@ class Bandit():
                 successes = self.periods['successes'][period_id][option_id]
                 if distance < self.cutoff:
                     if self.shape == 'constant':
+                        # Equal weight for every period
                         weight = 1
                     elif self.shape == 'linear':
+                        # Weight linearly decreases with distance
                         weight = 1 - distance / self.cutoff
                     elif self.shape == 'degressive':
+                        # Weight decrease (slope) shrinks with distance
                         weight = 2 * self.cutoff / (distance + self.cutoff) - 1
                     elif self.shape == 'progressive':
+                        # Weight decrease (slope) grows over distance
                         weight = -(distance / self.cutoff) ** 2 + 1
                     trial_weights[option_id] += trials * weight
                     success_weights[option_id] += successes * weight
         return [trial_weights, success_weights]
 
     def choose_options(self, choices):
+        """
+        Create Beta distributions for each option, draw one sample each,
+        choose the specified number of options with the largest values
+        """
+        # Calculate trials and success weighs for options if Bandit has memory,
+        # else use cumulated trials and successes
         if self.memory:
             trial_weights, success_weights = self.weigh_options()
         else:
@@ -66,10 +94,13 @@ class Bandit():
         return [idx for (idx, theta) in sorted(
             enumerate(sampled_theta), key=lambda x: x[1])[-choices:]]
 
-    def repeat_choice(self, choices=1, repetitions=100):
+    def repeat_choice(self, choices, repetitions):
+        """
+        Repeat choosing process and return aggregate outcome
+        """
         # Initialize option_counts array with zeros
         option_counts = np.zeros(shape=(self.num_options,), dtype=int)
-        # Choose best option and increment it's count value
+        # Choose best options and increment respective counts
         for _ in range(repetitions):
             options = self.choose_options(choices=choices)
             for option in options:
--- split.py
+++ split.py
@@ -1,9 +1,12 @@
 import numpy as np
 from scipy.stats import chi2_contingency
-# from scipy.stats import fisher_exact
 
 
 class Split():
+    """
+    Split instance holds cumulated trials, successes and failures
+    as well as current p-value; expects num_options to initialize
+    """
     def __init__(self, num_options):
         self.num_options = num_options
         self.trials = np.zeros(shape=(self.num_options,), dtype=int)
@@ -12,16 +15,21 @@ class Split():
         self.p_value = 1.0
 
     def add_results(self, option_id, trials, successes):
+        """
+        Add trials, successes and failures from last period to one option
+        """
         self.trials[option_id] = self.trials[option_id] + trials
         self.successes[option_id] = self.successes[option_id] + successes
         self.failures[option_id] = \
             self.failures[option_id] + trials - successes
 
     def calculate_p_value(self):
+        """
+        Calculate p-value from contingency table (observations)
+        with successes and failures
+        """
         observations = []
         for i in range(self.num_options):
             observations.append([self.successes[i], self.failures[i]])
         self.p_value = \
             chi2_contingency(observed=observations, correction=False)[1]
-        # Alternative p-value calculation with Fisher's exact test:
-        # fisher_exact(table=observations, alternative='two-sided')[1]
